29bfbce80072d2cba040e6b466084914374e25d8
pinke <pinke.yu7@gmail.com>
2022-06-13 10:34:18 +0800

    Feature: remove standard for model

--- const.go
+++ const.go
@@ -1,9 +1,9 @@
 package YatingTtsSdk
 
 const (
-	ModelStandardFemale1 = "zh_en_standard_female_1"
-	ModelStandardFemale2 = "zh_en_standard_female_2"
-	ModelStandardMale1   = "zh_en_standard_male_1"
+	ModelFemale1 = "zh_en_female_1"
+	ModelFemale2 = "zh_en_female_2"
+	ModelMale1   = "zh_en_male_1"
 
 	TypeText = "text"
 	TypeSsml = "ssml"
@@ -15,7 +15,7 @@ const (
 	SampleRate22k = "22K"
 )
 
-var Model = []string{ModelStandardFemale1, ModelStandardFemale2, ModelStandardMale1}
+var Model = []string{ModelFemale1, ModelFemale2, ModelMale1}
 var Type = []string{TypeText, TypeSsml}
 var Encoding = []string{EncodingMp3, EncodingLinear16}
 var SampleRate = []string{SampleRate16k}
--- example/main.go
+++ example/main.go
@@ -10,7 +10,7 @@ func main() {
 
 	text := "歡迎使用雅婷逐字稿。"
 	inputType := ttsClient.TypeText
-	model := ttsClient.ModelStandardFemale1
+	model := ttsClient.ModelFemale1
 	encoding := ttsClient.EncodingLinear16
 	sampleRate := ttsClient.SampleRate16k
 	fileName := "example"
