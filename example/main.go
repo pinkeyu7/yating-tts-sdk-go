a446f41e4c5ccf9a47d4559db18709d6c9d33578
pinke <pinke.yu7@gmail.com>
2023-10-20 14:03:52 +0800

    Update main.go

--- example/main.go
+++ example/main.go
@@ -10,13 +10,16 @@ func main() {
 
 	text := "歡迎使用雅婷逐字稿。"
 	inputType := ttsClient.TypeText
-	model := ttsClient.ModelFemale1
+	model := ttsClient.ModelZhEnFemale1
 	encoding := ttsClient.EncodingLinear16
 	sampleRate := ttsClient.SampleRate16k
+	speed := 1.0
+	pitch := 1.0
+	energy := 1.0
 	fileName := "example"
 
 	client := ttsClient.NewClient(url, key)
-	err := client.Synthesize(text, inputType, model, encoding, sampleRate, fileName)
+	err := client.Synthesize(text, inputType, model, encoding, sampleRate, speed, pitch, energy, fileName)
 	if err != nil {
 		panic(err)
 	}
